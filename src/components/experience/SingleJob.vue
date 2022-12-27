40691880ba9b4700decea30d58734ba9b8b44f54
Roialfassi <roialfassi@gmail.com>
2022-12-27 11:02:36 +0200

    Update SingleJob.vue

--- src/components/experience/SingleJob.vue
+++ src/components/experience/SingleJob.vue
@@ -12,7 +12,7 @@
       </h5>
       <ul data-aos="fade-left">
         <li v-for="desc in job.description" :key="desc.id">
-          {{ desc.description }};
+          {{ desc.description }}
         </li>
       </ul>
     </div>
